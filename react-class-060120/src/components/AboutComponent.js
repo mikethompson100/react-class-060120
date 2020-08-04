6b353e9e081ffbcd2b045f5fa6bd90894bb184b0
Thompson, Michael <michael.a.thompson@capgemini.com>
2020-08-04 09:21:45 -0400

    Completed last lesson

--- react-class-060120/src/components/AboutComponent.js
+++ react-class-060120/src/components/AboutComponent.js
@@ -2,27 +2,31 @@
 import { Breadcrumb, BreadcrumbItem, Card, CardBody, CardHeader, Media } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import { baseUrl } from '../shared/baseUrl';
+import { FadeTransform, Fade, Stagger } from 'react-animation-components';
 
 function About(props) {
 
    const leaders = props.leaders.leaders.map((leader) => {
-      return <RenderLeader key={leader.id} leader={leader} />;
+      return <RenderLeader key={leader.id} leader={leader} />
    });
 
    function RenderLeader({leader}) {
-      return (
-         <div key={leader.id} className="col-12 mt-5">
-            <Media tag="li">
-               <Media left middle>
-                  <Media object src={baseUrl + leader.image} alt={leader.name} />
-               </Media>
-               <Media body className="ml-5">
-                  <Media heading>{leader.name}</Media>
-                  <p>{leader.designation}</p>
-                  <p>{leader.description}</p>
-               </Media>
-            </Media>
-         </div>
+      return (    
+         <Fade in>
+            <div key={leader.id} className="col-12 mt-5">  
+               
+                     <Media tag="li">
+                        <Media left middle>
+                           <Media object src={baseUrl + leader.image} alt={leader.name} />
+                        </Media>
+                        <Media body className="ml-5">
+                           <Media heading>{leader.name}</Media>
+                           <p>{leader.designation}</p>
+                           <p>{leader.description}</p>
+                        </Media>
+                     </Media>                     
+            </div>    
+         </Fade>       
       )
    }
 
@@ -81,9 +85,11 @@ function About(props) {
                <h2>Corporate Leadership</h2>
             </div>
             <div className="col-12">
-               <Media list>
-                  {leaders}
-               </Media>
+               <Stagger in>
+                  <Media list>
+                     {leaders}
+                  </Media>
+               </Stagger>
             </div>
          </div>
       </div>
--- react-class-060120/src/components/ContactComponent.js
+++ react-class-060120/src/components/ContactComponent.js
@@ -19,6 +19,9 @@ class Contact extends Component {
    handleSubmit(values) {
       console.log('Current State is: ' + JSON.stringify(values));
       alert('Current State is: ' + JSON.stringify(values));
+
+      this.props.postFeedback(this.props.dishId, values.firstname, values.lastname,
+         values.telnum, values.email, values.agree, values.contactType, values.message);
       this.props.resetFeedbackForm();
    }
 
