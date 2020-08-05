27bf0853d07d7a9419873635e5e0387e8ff03436
Thompson, Michael <michael.a.thompson@capgemini.com>
2020-08-05 09:41:23 -0400

    Committed first deployment test

---
+++ json-server/public/precache-manifest.3fd5b3fb9c0515b27f7e8ddec9651a1b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2286e723fe6abdb22221534e3e36d545",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6236d049f933dc427cf3",
+    "url": "/static/css/2.4173b2d3.chunk.css"
+  },
+  {
+    "revision": "5a7f4c7b79c60909e0e4",
+    "url": "/static/css/main.50fbfd7b.chunk.css"
+  },
+  {
+    "revision": "6236d049f933dc427cf3",
+    "url": "/static/js/2.5fc572aa.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/static/js/2.5fc572aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a7f4c7b79c60909e0e4",
+    "url": "/static/js/main.85272760.chunk.js"
+  },
+  {
+    "revision": "146c7ab71709df3d46e5",
+    "url": "/static/js/runtime-main.bd309150.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
